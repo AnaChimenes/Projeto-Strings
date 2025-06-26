5fd8cd5131b06d69e9b6e5e8220f70cb8dd81c32
AnaChimenes <ana.chimenes.pinto@escola.pr.gov.br>
2025-06-26 11:28:28 +0000

    comço do trabalho

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoPalavra = document.querySelector('#botao-palavrachave');
+
+botaoPalavra.addEventListener('click', mostrarPalavrasChave);
+
+function mostrarPalavrasChave (){
+    alert ('fui clicado');
+}
