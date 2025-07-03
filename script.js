fdc975cebeded8a0ce7c834981709037d82929d5
AnaChimenes <ana.chimenes.pinto@escola.pr.gov.br>
2025-07-03 11:01:49 +0000

    aula 03

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const botaoPalavra = document.querySelector('#botao-palavrachave');
 botaoPalavra.addEventListener('click', mostrarPalavrasChave);
 
 function mostrarPalavrasChave (){
-    alert ('fui clicado');
+    const texto = document.querySelector('#entrada-de-texto').value; 
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+
+    campoResultado.textContent = texto;
 }
