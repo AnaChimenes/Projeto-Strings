aafae784be7a91db0406ac47616dccbf05ee46a4
AnaChimenes <ana.chimenes.pinto@escola.pr.gov.br>
2025-07-03 11:20:22 +0000

    aula 03 parte 2

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ botaoPalavra.addEventListener('click', mostrarPalavrasChave);
 function mostrarPalavrasChave (){
     const texto = document.querySelector('#entrada-de-texto').value; 
     const campoResultado = document.querySelector('#resultado-palavrachave');
-
-    campoResultado.textContent = texto;
+    const palavras = texto.split(" ");
+    
+    campoResultado.textContent = palavras.join(", ");
 }
